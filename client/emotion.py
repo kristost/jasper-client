9d23785e94ee3910dcc86b029ffbd035a610524f
Kris Tost <tost.kris@gmail.com>
2017-11-20 22:28:22 +0800

    Added support for emo detection using openXBOW

--- client/emotion.py
+++ client/emotion.py
@@ -19,11 +19,17 @@ class Emotion(object):
         self._model = pickle.load(open('Model.pkl', 'rb'))
         self._scaler = pickle.load(open('Scaler.pkl', 'rb'))
         self._encoder = pickle.load(open('Encoder.pkl', 'rb'))
+        
+        self._xbow_model, self._xbow_scaler, self._xbow_encoder = pickle.load(open('openXBOW/emodb_ComParE_2016_xbow.pkl', 'rb'))
+
         # TODO: Use jasper config (yaml) file to retrieve openSMILE path/bin location
 
         self._opensmile_path = '/home/pi/openSMILE/opensmile-2.3.0/'
         self._bin_path = 'inst/bin/SMILExtract'
         self._config_path = 'config/ComParE_2016.conf'
+
+        self._openxbow_path = '/home/pi/github/openXBOW/'
+        self._openxbow_bin = 'openXBOW.jar'
 
         self._sessionLogging = sessionLogging
         if self._sessionLogging:
@@ -40,11 +46,15 @@ class Emotion(object):
         if self._sessionLogging == True:
             timestamp = jasperpath.get_timestamp()
             output = self._sessionRoot + '/' + timestamp + '.arff'
+            lld_output = self._sessionRoot + '/' + timestamp + '.lld.arff'
         
         args = [self._opensmile_path + self._bin_path, ' -noconsoleoutput 1 -appendarff 0',
                 ' -C ', self._opensmile_path + self._config_path,
                 ' -I ', input,
-                ' -O ', output]
+                ' -O ', output,
+                ' -lldarffoutput ', lld_output,
+                ' -appendarfflld 0 ',
+                ' -timestamparff 1']
 
         self._logger.debug(args)
         cmd = ''.join(args)
@@ -62,7 +72,161 @@ class Emotion(object):
             self._logger.info("Copying WAV file '{}' to '{}'".format(input, dest))
             shutil.copyfile(input, dest)
         
+        return (p_status, output, lld_output)
+
+    def featuriseXBOW(self, input, output):
+
+        if self._sessionLogging == True:
+            timestamp = jasperpath.get_timestamp()
+            output = self._sessionRoot + '/' + timestamp + '.xbow.arff'
+        
+        args = ['java -jar ', self._openxbow_path + '/' + self._openxbow_bin,
+                ' -b ', 'openXBOW/emodb_ComParE_2016.codebook',
+                ' -i ', input,
+                ' -o ', output,
+                ' -noLabels ',
+                ' -attributes nt1[65]2[65]c']
+
+        self._logger.debug(args)
+        cmd = ''.join(args)
+
+        start_time = timeit.default_timer()
+
+        p = subprocess.Popen(cmd, shell=True, bufsize=-1)
+        p_status = p.wait()
+
+        elapsed = timeit.default_timer() - start_time
+        self._logger.info('Total elapsed time for openXBOW feature extraction: {}'.format(str(timedelta(seconds=elapsed))))
+        
+        #if p_status == 0 and self._sessionLogging:
+        #    dest = self._sessionRoot + '/' + timestamp + '.wav'
+        #    self._logger.info("Copying WAV file '{}' to '{}'".format(input, dest))
+        #    shutil.copyfile(input, dest)
+        
         return (p_status, output)
+
+
+    def predict_no_pandas(self, feature_file, event_type):
+        '''
+        # This function doesn't use pandas dataframes, so should be faster without it
+        '''
+        func_start_time = timeit.default_timer()
+
+        start_time = timeit.default_timer()
+
+        self._logger.info('Loading data from ARFF file {}'.format(feature_file))
+
+        data = liacarff.load(open(feature_file))
+        self._logger.info('ARFF file loaded.')
+
+        elapsed = timeit.default_timer() - start_time
+        self._logger.info('Total elapsed time to load ARFF: {}'.format(str(timedelta(seconds=elapsed))))
+
+        start_time = timeit.default_timer()
+
+        self._logger.info('Processing features from ARFF into list/numpy array.')
+        X = data['data'][0]
+        self._logger.info('Found {} features'.format(len(X)))
+
+        if len(X) == 6376:
+            X = X[2:-1]
+        elif len(X) == 6375:
+            X = X[1:-1]
+            print('Features truncated to {}'.format(len(X)))
+        elif len(X) == 6374:
+            X = X[1:]
+            print('Features truncated to {}'.format(len(X)))
+
+        X = np.reshape(X, (1,-1))
+        X = self._scaler.transform(X)
+        
+        elapsed = timeit.default_timer() - start_time
+        self._logger.info('Total elapsed time to create/scale list/numpy array: {}'.format(str(timedelta(seconds=elapsed))))
+
+        start_time = timeit.default_timer()
+
+        self._logger.info('Making prediction on emotion...')
+        prediction = self._model.predict(X)
+        label = self._encoder.inverse_transform(prediction)
+        if self._sessionLogging:
+            #print(feature_file)
+            #print(os.path.basename(feature_file))
+            #print(os.path.splitext(os.path.basename(feature_file)))
+            timestamp, _ = os.path.splitext(os.path.basename(feature_file))
+            #print(timestamp)
+            with open(self._sessionRoot + '/' + 'emotions.csv', 'ab') as f:
+                f.write(','.join([timestamp, 'openSMILE', event_type, str(prediction[0]), label[0], '\n']))
+
+        self._logger.info('Emotion predicted: {}'.format((prediction, label)))
+
+        elapsed = timeit.default_timer() - start_time
+        self._logger.info('Total elapsed time to make a prediction: {}'.format(str(timedelta(seconds=elapsed))))
+
+        elapsed = timeit.default_timer() - func_start_time
+        self._logger.info('Total elapsed time for function: {}'.format(str(timedelta(seconds=elapsed))))
+
+        return (prediction, label)
+
+
+    def predictXBOW(self, feature_file, event_type):
+        '''
+        # This function doesn't use pandas dataframes, so should be faster without it
+        '''
+        func_start_time = timeit.default_timer()
+
+        start_time = timeit.default_timer()
+
+        self._logger.info('Loading data from XBOW ARFF file {}'.format(feature_file))
+
+        data = liacarff.load(open(feature_file))
+        self._logger.info('XBOW ARFF file loaded.')
+
+        elapsed = timeit.default_timer() - start_time
+        self._logger.info('Total elapsed time to load XBOW ARFF: {}'.format(str(timedelta(seconds=elapsed))))
+
+        start_time = timeit.default_timer()
+
+        self._logger.info('Processing features from XBOW ARFF into list/numpy array.')
+        X = data['data'][0]
+        self._logger.info('Found {} features'.format(len(X)))
+
+        #if len(X) == 6375:
+        #    X = X[1:-1]
+        #    print('Features truncated to {}'.format(len(X)))
+        #elif len(X) == 6374:
+        #    X = X[1:]
+        #    print('Features truncated to {}'.format(len(X)))
+
+        X = np.reshape(X, (1,-1))
+        X = self._xbow_scaler.transform(X)
+        
+        elapsed = timeit.default_timer() - start_time
+        self._logger.info('Total elapsed time to create/scale list/numpy array: {}'.format(str(timedelta(seconds=elapsed))))
+
+        start_time = timeit.default_timer()
+
+        self._logger.info('Making prediction on emotion...')
+        prediction = self._xbow_model.predict(X)
+        label = self._xbow_encoder.inverse_transform(prediction)
+        if self._sessionLogging:
+            #print(feature_file)
+            #print(os.path.basename(feature_file))
+            #print(os.path.splitext(os.path.basename(feature_file)))
+            timestamp, _ = os.path.splitext(os.path.basename(feature_file))
+            #print(timestamp)
+            with open(self._sessionRoot + '/' + 'emotions.csv', 'ab') as f:
+                f.write(','.join([timestamp, 'openXBOW', event_type, str(prediction[0]), label[0], '\n']))
+
+        self._logger.info('Emotion predicted: {}'.format((prediction, label)))
+
+        elapsed = timeit.default_timer() - start_time
+        self._logger.info('Total elapsed time to make a prediction: {}'.format(str(timedelta(seconds=elapsed))))
+
+        elapsed = timeit.default_timer() - func_start_time
+        self._logger.info('Total elapsed time for function: {}'.format(str(timedelta(seconds=elapsed))))
+
+        return (prediction, label)
+
 
     def predict(self, feature_file):
         
@@ -95,65 +259,6 @@ class Emotion(object):
         self._logger.info('Making prediction on emotion...')
         prediction = self._model.predict(X)
         label = self._encoder.inverse_transform(prediction)
-        self._logger.info('Emotion predicted: {}'.format((prediction, label)))
-
-        elapsed = timeit.default_timer() - start_time
-        self._logger.info('Total elapsed time to make a prediction: {}'.format(str(timedelta(seconds=elapsed))))
-
-        elapsed = timeit.default_timer() - func_start_time
-        self._logger.info('Total elapsed time for function: {}'.format(str(timedelta(seconds=elapsed))))
-
-        return (prediction, label)
-
-    def predict_no_pandas(self, feature_file, event_type):
-        '''
-        # This function doesn't use pandas dataframes, so should be faster without it
-        '''
-        func_start_time = timeit.default_timer()
-
-        start_time = timeit.default_timer()
-
-        self._logger.info('Loading data from ARFF file {}'.format(feature_file))
-
-        data = liacarff.load(open(feature_file))
-        self._logger.info('ARFF file loaded.')
-
-        elapsed = timeit.default_timer() - start_time
-        self._logger.info('Total elapsed time to load ARFF: {}'.format(str(timedelta(seconds=elapsed))))
-
-        start_time = timeit.default_timer()
-
-        self._logger.info('Processing features from ARFF into list/numpy array.')
-        X = data['data'][0]
-        self._logger.info('Found {} features'.format(len(X)))
-
-        if len(X) == 6375:
-            X = X[1:-1]
-            print('Features truncated to {}'.format(len(X)))
-        elif len(X) == 6374:
-            X = X[1:]
-            print('Features truncated to {}'.format(len(X)))
-
-        X = np.reshape(X, (1,-1))
-        X = self._scaler.transform(X)
-        
-        elapsed = timeit.default_timer() - start_time
-        self._logger.info('Total elapsed time to create/scale list/numpy array: {}'.format(str(timedelta(seconds=elapsed))))
-
-        start_time = timeit.default_timer()
-
-        self._logger.info('Making prediction on emotion...')
-        prediction = self._model.predict(X)
-        label = self._encoder.inverse_transform(prediction)
-        if self._sessionLogging:
-            #print(feature_file)
-            #print(os.path.basename(feature_file))
-            #print(os.path.splitext(os.path.basename(feature_file)))
-            timestamp, _ = os.path.splitext(os.path.basename(feature_file))
-            #print(timestamp)
-            with open(self._sessionRoot + '/' + 'emotions.csv', 'ab') as f:
-                f.write(','.join([timestamp, event_type, str(prediction[0]), label[0], '\n']))
-
         self._logger.info('Emotion predicted: {}'.format((prediction, label)))
 
         elapsed = timeit.default_timer() - start_time
